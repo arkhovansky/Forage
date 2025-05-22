d148ce1425c39dbd58a2078253f6e3c54a0f3221
Alexander Khovansky <arkhovansky@yandex.com>
2025-05-22 22:48:30 +0300

    [Code style] Lib/VisualGrid: corrected whitespaces

--- Assets/Scripts/Lib/VisualGrid/VisualHexGrid.cs
+++ Assets/Scripts/Lib/VisualGrid/VisualHexGrid.cs
@@ -10,41 +10,40 @@ namespace Lib.VisualGrid {
 
 
 
- public class VisualHexGrid
- 	: HexGrid
- {
- 	public HexLayout Layout { get; }
+public class VisualHexGrid
+	: HexGrid
+{
+	public HexLayout Layout { get; }
 
 
-    //----------------------------------------------------------------------------------------------
+	//----------------------------------------------------------------------------------------------
 
 
- 	public VisualHexGrid(HexLayout layout,
-                         uint width, uint height,
-                         HexGridLineOffset lineOffset)
- 		: base(width, height, layout.Orientation, lineOffset)
- 	{
-	    Layout = layout;
- 	}
+	public VisualHexGrid(HexLayout layout,
+	                     uint width, uint height,
+	                     HexGridLineOffset lineOffset)
+		: base(width, height, layout.Orientation, lineOffset)
+	{
+		Layout = layout;
+	}
 
 
- 	public VisualHexGrid(HexLayout layout, HexGrid grid)
- 		: base(grid.Width, grid.Height, layout.Orientation, grid.LineOffset)
-    {
-	    if (layout.Orientation != grid.Orientation)
-		    throw new ArgumentException();
-	    
-	    Layout = layout;
- 	}
+	public VisualHexGrid(HexLayout layout, HexGrid grid)
+		: base(grid.Width, grid.Height, layout.Orientation, grid.LineOffset)
+	{
+		if (layout.Orientation != grid.Orientation)
+			throw new ArgumentException();
+
+		Layout = layout;
+	}
 
 
-
-    public AxialPosition? GetAxialPosition(Vector2 point)
-    {
-	    AxialPosition freePos = Layout.GetAxialPosition(point);
-	    return IsInside(freePos) ? freePos : null;
-    }
- }
+	public AxialPosition? GetAxialPosition(Vector2 point)
+	{
+		AxialPosition freePos = Layout.GetAxialPosition(point);
+		return IsInside(freePos) ? freePos : null;
+	}
+}
 
 
 
