b1bd52b0d78991bb47b61c1fb0a530d9db5b5310
Alexander Khovansky <arkhovansky@yandex.com>
2025-05-24 17:58:06 +0300

    [Code style] Small identifier case correction

--- Assets/Scripts/App/Game/Authoring/PrefabReferences.cs
+++ Assets/Scripts/App/Game/Authoring/PrefabReferences.cs
@@ -9,7 +9,7 @@ namespace App.Game.Authoring {
 
 public class PrefabReferences : MonoBehaviour
 {
-	public GameObject campPrefab;
+	public GameObject CampPrefab;
 
 
 	public class Baker : Baker<PrefabReferences>
@@ -19,7 +19,7 @@ public class PrefabReferences : MonoBehaviour
 			var entity = GetEntity(TransformUsageFlags.None);
 			AddComponent(entity,
 				new Ecs.Components.Singletons.PrefabReferences {
-					Camp = GetEntity(authoring.campPrefab, TransformUsageFlags.Dynamic)
+					Camp = GetEntity(authoring.CampPrefab, TransformUsageFlags.Dynamic)
 				});
 		}
 	}
