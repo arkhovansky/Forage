0643ebe62de29dd95e0389605a5025f157801fae
Alexander Khovansky <arkhovansky@yandex.com>
2025-05-24 21:57:10 +0300

    [Editor] Nullability handling in PrefabReferences' baker

--- Assets/Scripts/App/Game/Authoring/PrefabReferences.cs
+++ Assets/Scripts/App/Game/Authoring/PrefabReferences.cs
@@ -9,13 +9,18 @@ namespace App.Game.Authoring {
 
 public class PrefabReferences : MonoBehaviour
 {
-	public GameObject CampPrefab;
+	public GameObject CampPrefab = null!;
 
 
 	public class Baker : Baker<PrefabReferences>
 	{
 		public override void Bake(PrefabReferences authoring)
 		{
+			DependsOn(authoring.CampPrefab);
+
+			if (authoring.CampPrefab == null)
+				throw new System.NullReferenceException($"{nameof(PrefabReferences)}.{nameof(CampPrefab)} is null");
+
 			var entity = GetEntity(TransformUsageFlags.None);
 			AddComponent(entity,
 				new Ecs.Components.Singletons.PrefabReferences {
