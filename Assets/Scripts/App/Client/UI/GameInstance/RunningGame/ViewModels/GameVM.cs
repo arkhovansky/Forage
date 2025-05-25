aac6619b7e2c365143d97522dc3bbebc1a83c41b
Alexander Khovansky <arkhovansky@yandex.com>
2025-05-25 13:39:40 +0300

    [Refactoring] Moved child view models out of GameVM.cs

--- Assets/Scripts/App/Client/UI/GameInstance/RunningGame/ViewModels/GameVM.cs
+++ Assets/Scripts/App/Client/UI/GameInstance/RunningGame/ViewModels/GameVM.cs
@@ -5,16 +5,10 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Properties;
 
-using Lib.Grid;
-
 using App.Client.Framework.UICore.HighLevel;
 using App.Client.Framework.UICore.Mvvm;
 using App.Game.ECS.BandMember.Components;
-using App.Game.ECS.Components;
 using App.Game.ECS.GameTime.Components;
-using App.Game.ECS.Resource.Plant.Components;
-using App.Game.ECS.Terrain.Components;
-using App.Game.ECS.UI.HoveredTile.Components;
 using App.Services.BandMembers;
 using App.Services.Resources;
 using App.Services.Terrain;
@@ -117,145 +111,6 @@ public class GameVM : IViewModel
 		}
 	}
 }
-
-
-
-public class TileInfoVM : IViewModel
-{
-	[CreateProperty]
-	public string TerrainType { get; private set; }
-
-
-	public ResourceInfoVM ResourceInfoVM { get; }
-
-
-
-	private readonly ITerrainTypeRepository _terrainTypeRepository;
-
-
-
-	public TileInfoVM(ITerrainTypeRepository terrainTypeRepository,
-	                  IResourceTypeRepository resourceTypeRepository)
-	{
-		_terrainTypeRepository = terrainTypeRepository;
-
-		TerrainType = string.Empty;
-
-		ResourceInfoVM = new ResourceInfoVM(resourceTypeRepository);
-	}
-
-
-	public void Update()
-	{
-		var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
-		var query = entityManager.CreateEntityQuery(ComponentType.ReadOnly<HoveredTileEntity>());
-
-		if (query.TryGetSingleton<HoveredTileEntity>(out var hoveredTileEntityComponent)) {
-			var hoveredTileEntity = hoveredTileEntityComponent.Entity;
-
-			var terrainTileComponent = entityManager.GetComponentData<TerrainTile>(hoveredTileEntity);
-			var terrainTypeId = terrainTileComponent.TerrainType;
-
-			var terrainType = _terrainTypeRepository.Get(terrainTypeId);
-			TerrainType = terrainType.Name;
-
-
-			var position = entityManager.GetComponentData<TilePosition>(hoveredTileEntity).Position;
-
-			if (TryGetResource(position, out Entity resourceEntity))
-				ResourceInfoVM.Show(resourceEntity);
-			else
-				ResourceInfoVM.Hide();
-		}
-		else {
-			TerrainType = string.Empty;
-			ResourceInfoVM.Hide();
-		}
-	}
-
-
-	private bool TryGetResource(AxialPosition position, out Entity resourceEntity)
-	{
-		var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
-		var query = entityManager.CreateEntityQuery(
-			ComponentType.ReadOnly<TilePosition>(),
-			ComponentType.ReadOnly<PlantResource>());
-
-		var positions = query.ToComponentDataArray<TilePosition>(Allocator.Temp);
-		var entities = query.ToEntityArray(Allocator.Temp);
-
-		for (var i = 0; i < positions.Length; i++) {
-			if (positions[i].Position == position) {
-				resourceEntity = entities[i];
-				return true;
-			}
-		}
-
-		resourceEntity = Entity.Null;
-		return false;
-	}
-}
-
-
-
-public class ResourceInfoVM : IViewModel
-{
-	[CreateProperty]
-	public bool IsVisible { get; private set; }
-
-	[CreateProperty]
-	public string Name { get; private set; }
-
-	[CreateProperty]
-	public uint PotentialBiomass { get; private set; }
-
-	[CreateProperty]
-	public string RipenessPeriod { get; private set; }
-
-	[CreateProperty]
-	public uint RipeBiomass { get; private set; }
-
-
-
-	private readonly IResourceTypeRepository _resourceTypeRepository;
-
-
-
-	public ResourceInfoVM(IResourceTypeRepository resourceTypeRepository)
-	{
-		_resourceTypeRepository = resourceTypeRepository;
-
-		Name = string.Empty;
-		RipenessPeriod = string.Empty;
-	}
-
-
-	public void Show(Entity resourceEntity)
-	{
-		var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
-		var resource = entityManager.GetComponentData<PlantResource>(resourceEntity);
-
-		var resourceType = _resourceTypeRepository.Get(resource.TypeId);
-
-		Name = resourceType.Name;
-		PotentialBiomass = (uint) resource.PotentialBiomass;
-		RipenessPeriod = resource.RipenessPeriod.Month.ToString();
-
-		RipeBiomass = (uint) entityManager.GetComponentData<RemainingRipeBiomass>(resourceEntity).Value;
-
-		IsVisible = true;
-	}
-
-
-	public void Hide()
-	{
-		IsVisible = false;
-	}
-}
-
 
 
 
---
+++ Assets/Scripts/App/Client/UI/GameInstance/RunningGame/ViewModels/ResourceInfoVM.cs
@@ -0,0 +1,72 @@
+﻿using Unity.Entities;
+using Unity.Properties;
+
+using App.Client.Framework.UICore.Mvvm;
+using App.Game.ECS.Resource.Plant.Components;
+using App.Services.Resources;
+
+
+
+namespace App.Client.UI.GameInstance.RunningGame {
+
+
+
+public class ResourceInfoVM : IViewModel
+{
+	[CreateProperty]
+	public bool IsVisible { get; private set; }
+
+	[CreateProperty]
+	public string Name { get; private set; }
+
+	[CreateProperty]
+	public uint PotentialBiomass { get; private set; }
+
+	[CreateProperty]
+	public string RipenessPeriod { get; private set; }
+
+	[CreateProperty]
+	public uint RipeBiomass { get; private set; }
+
+
+
+	private readonly IResourceTypeRepository _resourceTypeRepository;
+
+
+
+	public ResourceInfoVM(IResourceTypeRepository resourceTypeRepository)
+	{
+		_resourceTypeRepository = resourceTypeRepository;
+
+		Name = string.Empty;
+		RipenessPeriod = string.Empty;
+	}
+
+
+	public void Show(Entity resourceEntity)
+	{
+		var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+		var resource = entityManager.GetComponentData<PlantResource>(resourceEntity);
+
+		var resourceType = _resourceTypeRepository.Get(resource.TypeId);
+
+		Name = resourceType.Name;
+		PotentialBiomass = (uint) resource.PotentialBiomass;
+		RipenessPeriod = resource.RipenessPeriod.Month.ToString();
+
+		RipeBiomass = (uint) entityManager.GetComponentData<RemainingRipeBiomass>(resourceEntity).Value;
+
+		IsVisible = true;
+	}
+
+
+	public void Hide()
+	{
+		IsVisible = false;
+	}
+}
+
+
+
+}
