59f4344b9e2e8d9fa6fa87baa23feca116302d9d
Alexander Khovansky <arkhovansky@yandex.com>
2025-05-27 11:24:28 +0300

    [Refactoring] Extracted BandMembersVM from GameVM

---
+++ Assets/Scripts/App/Client/UI/GameInstance/RunningGame/ViewModels/BandMembersVM.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine.Assertions;
+
+using App.Client.Framework.UICore.Mvvm;
+using App.Game.ECS.BandMember.Components;
+using App.Services.BandMembers;
+
+
+
+namespace App.Client.UI.GameInstance.RunningGame {
+
+
+
+public class BandMembersVM : IViewModel
+{
+	public List<BandMemberVM> BandMembers { get; }
+
+
+	private readonly IBandMemberTypeRepository _bandMemberTypeRepository;
+
+
+
+	public BandMembersVM(IBandMemberTypeRepository bandMemberTypeRepository)
+	{
+		BandMembers = new List<BandMemberVM>();
+
+		_bandMemberTypeRepository = bandMemberTypeRepository;
+	}
+
+
+	public void Update()
+	{
+		var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+		var query = entityManager.CreateEntityQuery(
+			ComponentType.ReadOnly<BandMember>(),
+			ComponentType.ReadOnly<Human>(),
+			ComponentType.ReadOnly<Forager>());
+
+		var bandMembers = query.ToComponentDataArray<BandMember>(Allocator.Temp);
+		var humans = query.ToComponentDataArray<Human>(Allocator.Temp);
+		var foragers = query.ToComponentDataArray<Forager>(Allocator.Temp);
+
+		if (BandMembers.Count == 0) {
+			for (var i = 0; i < bandMembers.Length; i++) {
+				BandMembers.Add(new BandMemberVM() {
+					Id = bandMembers[i].Id,
+					Gender = _bandMemberTypeRepository.Get(humans[i].TypeId).Gender.ToString(),
+					Assignment = foragers[i].Activity.ToString()
+				});
+			}
+		}
+		else {
+			Assert.IsTrue(BandMembers.Count == bandMembers.Length);
+
+			for (var i = 0; i < bandMembers.Length; i++) {
+				var bandMemberVM = BandMembers.Find(x => x.Id == bandMembers[i].Id);
+
+				bandMemberVM.Gender = _bandMemberTypeRepository.Get(humans[i].TypeId).Gender.ToString();
+				bandMemberVM.Assignment = foragers[i].Activity.ToString();
+			}
+		}
+	}
+}
+
+
+
+}
--- Assets/Scripts/App/Client/UI/GameInstance/RunningGame/ViewModels/GameVM.cs
+++ Assets/Scripts/App/Client/UI/GameInstance/RunningGame/ViewModels/GameVM.cs
@@ -1,13 +1,8 @@
-﻿using System.Collections.Generic;
-
-using Unity.Assertions;
-using Unity.Collections;
-using Unity.Entities;
+﻿using Unity.Entities;
 using Unity.Properties;
 
 using App.Client.Framework.UICore.HighLevel;
 using App.Client.Framework.UICore.Mvvm;
-using App.Game.ECS.BandMember.Components;
 using App.Game.ECS.GameTime.Components;
 using App.Services.BandMembers;
 using App.Services.Resources;
@@ -24,16 +19,12 @@ public class GameVM : IViewModel
 	[CreateProperty]
 	public string Month { get; private set; }
 
-	public List<BandMemberVM> BandMembers { get; }
+	public BandMembersVM BandMembersVM { get; }
 
 	public TileInfoVM TileInfoVM { get; }
 
 	public EnterPlaceCampMode_CommandVM EnterPlaceCampModeCommand { get; }
 	public EndTurn_CommandVM EndTurnCommand { get; }
-
-
-
-	private readonly IBandMemberTypeRepository _bandMemberTypeRepository;
 
 
 
@@ -45,7 +36,7 @@ public class GameVM : IViewModel
 	{
 		Month = string.Empty;
 
-		BandMembers = new List<BandMemberVM>();
+		BandMembersVM = new BandMembersVM(bandMemberTypeRepository);
 
 		TileInfoVM = new TileInfoVM(terrainTypeRepository, resourceTypeRepository);
 
@@ -53,15 +44,13 @@ public class GameVM : IViewModel
 			() => commandRouter.EmitCommand(new EnterPlaceCampMode(), controller));
 		EndTurnCommand = new EndTurn_CommandVM(
 			() => commandRouter.EmitCommand(new EndTurnCommand(), controller));
-
-		_bandMemberTypeRepository = bandMemberTypeRepository;
 	}
 
 
 	public void Update()
 	{
 		UpdateYearPeriod();
-		UpdateBandMembers();
+		BandMembersVM.Update();
 		TileInfoVM.Update();
 	}
 
@@ -74,41 +63,6 @@ public class GameVM : IViewModel
 		var yearPeriod = query.GetSingleton<CurrentYearPeriod>();
 
 		Month = yearPeriod.Value.Month.ToString();
-	}
-
-
-	private void UpdateBandMembers()
-	{
-		var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-
-		var query = entityManager.CreateEntityQuery(
-			ComponentType.ReadOnly<BandMember>(),
-			ComponentType.ReadOnly<Human>(),
-			ComponentType.ReadOnly<Forager>());
-
-		var bandMembers = query.ToComponentDataArray<BandMember>(Allocator.Temp);
-		var humans = query.ToComponentDataArray<Human>(Allocator.Temp);
-		var foragers = query.ToComponentDataArray<Forager>(Allocator.Temp);
-
-		if (BandMembers.Count == 0) {
-			for (var i = 0; i < bandMembers.Length; i++) {
-				BandMembers.Add(new BandMemberVM() {
-					Id = bandMembers[i].Id,
-					Gender = _bandMemberTypeRepository.Get(humans[i].TypeId).Gender.ToString(),
-					Assignment = foragers[i].Activity.ToString()
-				});
-			}
-		}
-		else {
-			Assert.IsTrue(BandMembers.Count == bandMembers.Length);
-
-			for (var i = 0; i < bandMembers.Length; i++) {
-				var bandMemberVM = BandMembers.Find(x => x.Id == bandMembers[i].Id);
-
-				bandMemberVM.Gender = _bandMemberTypeRepository.Get(humans[i].TypeId).Gender.ToString();
-				bandMemberVM.Assignment = foragers[i].Activity.ToString();
-			}
-		}
 	}
 }
 
