a0a53a302f413f94c46f7494a0a13c6a406e876c
Alexander Khovansky <arkhovansky@yandex.com>
2025-06-04 23:29:54 +0300

    [Cleanup] TerrainInitializer: removed commented code

--- Assets/Scripts/App/Services/Terrain/TerrainInitializer.cs
+++ Assets/Scripts/App/Services/Terrain/TerrainInitializer.cs
@@ -60,8 +60,6 @@ public class TerrainInitializer : ITerrainInitializer
 		};
 
 
-		// var ecb = new EntityCommandBuffer(Allocator.TempJob);
-
 		var prototype = entityManager.CreateEntity();
 		entityManager.AddComponent<TerrainTile>(prototype);
 		entityManager.AddComponent<TilePosition>(prototype);
@@ -72,17 +70,12 @@ public class TerrainInitializer : ITerrainInitializer
 			renderMeshDescription,
 			renderMeshArray,
 			MaterialMeshInfo.FromRenderMeshArrayIndices(0, 0));
-		// entityManager.AddComponent<MaterialColor>(prototype);
 		entityManager.AddComponent<Static>(prototype);
-		// entityManager.SetComponentData(prototype, new LocalToWorld { Value = float4x4.identity });
-
-		// var bounds = new RenderBounds { Value = tileMesh.bounds.ToAABB() };
 
 		var terrainTileCount = tileTerrainTypes.Count;
 
 		var clonedEntities = new NativeArray<Entity>(terrainTileCount, Allocator.Temp);
 		entityManager.Instantiate(prototype, clonedEntities);
-
 
 		for (uint i = 0; i < terrainTileCount; ++i) {
 			var entity = clonedEntities[(int)i];
@@ -97,77 +90,17 @@ public class TerrainInitializer : ITerrainInitializer
 			entityManager.SetComponentData(entity, _grid.GetCellLocalTransform(axialPosition));
 
 			entityManager.SetComponentData(entity, terrainType_To_MaterialMeshInfo[terrainTypeId]);
-
-			// float3 hsvColor = map.Tile(i).TerrainTypeId switch {
-			// 	3 => new float3(130, 80, 80),
-			// 	4 => new float3(70, 80, 80),
-			// 	5 => new float3(80, 80, 80),
-			// 	_ => new float3()
-			// };
-			// var rgb = Color.HSVToRGB(hsvColor.x, hsvColor.y, hsvColor.z);
-			// var color = new float4(rgb.r, rgb.g, rgb.b, 1);
-			// entityManager.SetComponentData(entity, new MaterialColor { Value = color });
-
-			// entityManager.SetComponentData(entity, bounds);
 		}
 
 		clonedEntities.Dispose();
 
 
-		// Spawn most of the entities in a Burst job by cloning a pre-created prototype entity,
-		// which can be either a Prefab or an entity created at run time like in this sample.
-		// This is the fastest and most efficient way to create entities at run time.
-		// var spawnJob = new SpawnJob {
-		// 	Prototype = prototype,
-		// 	Map = map,
-		// 	Ecb = ecb.AsParallelWriter(),
-		// 	MeshBounds = bounds,
-		// 	//ObjectScale = ObjectScale,
-		// };
-		//
-		// var spawnHandle = spawnJob.Schedule((int) map.TileCount, 128);
-		// bounds.Dispose(spawnHandle);
-		//
-		// spawnHandle.Complete();
-		//
-		// ecb.Playback(entityManager);
-		// ecb.Dispose();
-
 		entityManager.DestroyEntity(prototype);
 	}
 
 
-
 	//----------------------------------------------------------------------------------------------
 	// private
-
-
-	// [GenerateTestsForBurstCompatibility]
-	// private struct SpawnJob : IJobParallelFor
-	// {
-	// 	public Entity Prototype;
-	// 	public ITerrainMap Map;
-	// 	//public float ObjectScale;
-	// 	public EntityCommandBuffer.ParallelWriter Ecb;
-	//
-	// 	[ReadOnly] public NativeArray<RenderBounds> MeshBounds;
-	//
-	//
-	// 	public void Execute(int index)
-	// 	{
-	// 		var e = Ecb.Instantiate(index, Prototype);
-	//
-	// 		Ecb.SetComponent(index, e, Map.Tile((uint) index));
-	//
-	// 		Ecb.SetComponent(index, e, new LocalToWorld { Value = Map.Grid.GetCellLocalTransform((uint) index) });
-	// 		//Ecb.SetComponent(index, e, new MaterialColor() { Value = ComputeColor(index) });
-	// 		// MeshBounds must be set according to the actual mesh for culling to work.
-	// 		int materialIndex = (int) Map.Tile((uint) index).TerrainTypeId;
-	// 		Ecb.SetComponent(index, e, MaterialMeshInfo.FromRenderMeshArrayIndices(materialIndex, 0));
-	// 		Ecb.SetComponent(index, e, MeshBounds[0]);
-	// 	}
-	// }
-
 
 
 	private
