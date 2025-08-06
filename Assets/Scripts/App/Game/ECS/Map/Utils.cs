cb68c17033acac614ec95623fb808905e657de9c
Alexander Khovansky <arkhovansky@yandex.com>
2025-08-06 18:46:20 +0300

    step: Added EcsMap utility struct

---
+++ Assets/Scripts/App/Game/ECS/Map/Utils.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Entities;
+
+using Lib.Grid;
+
+using App.Game.ECS.Map.Components.Singletons;
+
+
+
+namespace App.Game.ECS.Map {
+
+
+
+public struct EcsMap
+{
+	private readonly RectangularHexMap _map;
+	private readonly DynamicBuffer<MapTileEntity> _tileIndexBuffer;
+
+
+
+	public EcsMap(RectangularHexMap map, DynamicBuffer<MapTileEntity> tileIndexBuffer)
+	{
+		_map = map;
+		_tileIndexBuffer = tileIndexBuffer;
+	}
+
+
+	public readonly Entity GetTileEntity(AxialPosition position)
+	{
+		return GetTileEntity(position, _map, _tileIndexBuffer);
+	}
+
+
+	public static Entity GetTileEntity(AxialPosition position,
+	                                   in RectangularHexMap map, in DynamicBuffer<MapTileEntity> tileIndexBuffer)
+	{
+		int cellIndex = (int) map.CellIndexFrom(position);
+		return tileIndexBuffer[cellIndex];
+	}
+}
+
+
+
+}
