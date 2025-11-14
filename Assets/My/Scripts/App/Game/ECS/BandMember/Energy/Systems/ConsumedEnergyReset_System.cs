ca8a3a1472eda2d4040337170e99463c11e31374
Alexander Khovansky <arkhovansky@yandex.com>
2025-11-14 19:39:11 +0300

    refactor(ecs): ConsumedEnergyReset_System now uses proper DayChanged event

--- Assets/My/Scripts/App/Game/ECS/BandMember/Energy/Systems/ConsumedEnergyReset_System.cs
+++ Assets/My/Scripts/App/Game/ECS/BandMember/Energy/Systems/ConsumedEnergyReset_System.cs
@@ -2,6 +2,7 @@
 using Unity.Entities;
 
 using App.Game.ECS.BandMember.Energy.Components;
+using App.Game.ECS.GameTime.Components.Events;
 using App.Game.ECS.SystemGroups;
 
 
@@ -14,13 +15,15 @@ namespace App.Game.ECS.BandMember.Energy.Systems {
 public partial struct ConsumedEnergyReset_System : ISystem
 {
 	[BurstCompile]
+	public void OnCreate(ref SystemState state)
+	{
+		state.RequireForUpdate<DayChanged>();
+	}
+
+
+	[BurstCompile]
 	public void OnUpdate(ref SystemState state)
 	{
-		var gameTime = SystemAPI.GetSingleton<GameTime.Components.GameTime>();
-
-		if (!gameTime.DayChanged)
-			return;
-
 		foreach (var foodConsumer
 		         in SystemAPI.Query<RefRW<FoodConsumer>>())
 		{
